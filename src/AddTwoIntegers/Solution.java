2c5268f8857e02974fea0fe34d45419472c93a8d
Abhinav Bhardwaj <abhinavbhardwaj68@gmail.com>
2023-01-21 13:54:31 +0530

    2235. Add Two Integers

---
+++ src/AddTwoIntegers/Solution.java
@@ -0,0 +1,15 @@
+package AddTwoIntegers;
+
+/**
+ * Created by IntelliJ IDEA
+ * User: Abhinav Bhardwaj
+ * Date: 21/01/23
+ * Time: 13:51
+ */
+
+
+public class Solution {
+    public int sum(int num1, int num2) {
+        return num1 + num2;
+    }
+}
