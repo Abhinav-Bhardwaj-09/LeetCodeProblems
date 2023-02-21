4f5022d234abf2266f8a1aea00f18ba01aae3a6b
Abhinav Bhardwaj <abhinavbhardwaj68@gmail.com>
2023-02-21 21:14:36 +0530

    1693. Daily Leads and Partners

---
+++ src/DailyLeadsAndPartners/Solution.sql
@@ -0,0 +1,1 @@
+SELECT date_id, make_name, COUNT(DISTINCT lead_id) AS unique_leads, COUNT( DISTINCT partner_id) AS unique_partners FROM DailySales GROUP BY date_id, make_name ORDER BY make_name DESC, date_id DESC;
