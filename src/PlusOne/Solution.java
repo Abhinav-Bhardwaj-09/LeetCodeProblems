7765cea92d3c1057c5ed0dc9b737becccb5f01ad
Abhinav Bhardwaj <abhinavbhardwaj68@gmail.com>
2026-01-22 21:01:01 +0530

    66. Plus One

--- src/PlusOne/Solution.java
+++ src/PlusOne/Solution.java
@@ -1,9 +1,33 @@
 package PlusOne;
 
-/** Created by IntelliJ IDEA User: Abhinav Bhardwaj Date: 21/03/23 Time: 22:51 */
+// Created by Abhinav Bhardwaj on 21/03/2023 22:51 using IntelliJ IDEA
 
 public class Solution {
+
   public int[] plusOne(int[] digits) {
+    int digitsLen = digits.length;
+    boolean carryForwardFlag = false;
+
+    for (int index = digitsLen - 1; index >= 0; index--) {
+      if (digits[index] == 9) {
+        digits[index] = 0;
+        carryForwardFlag = true;
+      } else {
+        carryForwardFlag = false;
+        digits[index]++;
+        break;
+      }
+    }
+
+    if (carryForwardFlag) {
+      digits = new int[digitsLen + 1];
+      digits[0] = 1;
+    }
+
+    return digits;
+  }
+
+  public int[] plusOne1(int[] digits) {
     int digitsLen = digits.length;
     boolean flag = false;
 
