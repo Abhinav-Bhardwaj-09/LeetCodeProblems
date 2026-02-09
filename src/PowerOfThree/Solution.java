fc301e787a2ceb58e7070201d4ac9c324ddf0bc0
Abhinav Bhardwaj <abhinavbhardwaj68@gmail.com>
2026-02-09 20:53:08 +0530

    326. Power of Three

---
+++ src/PowerOfThree/Solution.java
@@ -0,0 +1,23 @@
+package PowerOfThree;
+
+// Created by Abhinav Bhardwaj on 09/02/2026 20:46 using IntelliJ IDEA
+
+public class Solution {
+  public boolean isPowerOfThree(int n) {
+
+    double product = Math.pow(3, 0);
+
+    while (product <= n) {
+      if (product == n) {
+        return true;
+      } else {
+        product *= 3;
+      }
+    }
+
+    return false;
+  }
+}
+
+
+
