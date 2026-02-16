62fdc3200027c69838a1a3413e3febc0a664cfa3
Abhinav Bhardwaj <abhinavbhardwaj68@gmail.com>
2026-02-16 20:23:00 +0530

    217. Contains Duplicate - code reformatting and added constraint with problem statement

--- src/ContainsDuplicate/Solution.java
+++ src/ContainsDuplicate/Solution.java
@@ -2,27 +2,21 @@ package ContainsDuplicate;
 
 import java.util.HashSet;
 
-/**
- * Created by IntelliJ IDEA
- * User: Abhinav Bhardwaj
- * Date: 24/01/23
- * Time: 15:44
- */
-
+// Created by Abhinav Bhardwaj on 24/01/2023 15:44 using IntelliJ IDEA
 
 public class Solution {
-    public boolean containsDuplicate(int[] nums) {
-        if(nums == null || nums.length == 0) {
-            return false;
-        }
-
-        HashSet<Integer> numsHashSet = new HashSet<Integer>();
-        for(int element: nums) {
-            if(!numsHashSet.add(element)){
-                return true;
-            }
-        }
-
-        return false;
+  public boolean containsDuplicate(int[] nums) {
+    if (nums == null || nums.length == 0) {
+      return false;
     }
+
+    HashSet<Integer> numsHashSet = new HashSet<Integer>();
+    for (int element : nums) {
+      if (!numsHashSet.add(element)) {
+        return true;
+      }
+    }
+
+    return false;
+  }
 }
