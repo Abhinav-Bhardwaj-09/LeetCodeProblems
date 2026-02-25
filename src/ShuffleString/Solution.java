e0cbb0445879a7a901614e1b14fe67c0b05fffb8
Abhinav Bhardwaj <abhinavbhardwaj68@gmail.com>
2026-02-25 15:23:00 +0530

    1528. Shuffle String

---
+++ src/ShuffleString/Solution.java
@@ -0,0 +1,18 @@
+package ShuffleString;
+
+import java.util.Arrays;
+
+// Created by Abhinav Bhardwaj on 25/02/2026 22:47 using IntelliJ IDEA
+
+public class Solution {
+  public String restoreString(String s, int[] indices) {
+    char[] arr = new char[s.length()];
+    int count = 0;
+
+    for (int element : indices) {
+      arr[element] = s.charAt(count++);
+    }
+
+    return new String(arr);
+  }
+}
