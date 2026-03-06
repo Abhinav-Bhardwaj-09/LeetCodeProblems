aac11bf50290efea35ce35b99ac63fa66902b2b6
Abhinav Bhardwaj <abhinavbhardwaj68@gmail.com>
2026-03-06 15:23:00 +0530

    1431. Kids with the Greatest Number of Candies

--- src/KidsWithTheGreatestNumberOfCandies/Solution.java
+++ src/KidsWithTheGreatestNumberOfCandies/Solution.java
@@ -1,7 +1,20 @@
 package KidsWithTheGreatestNumberOfCandies;
- 
-// Created by Abhinav Bhardwaj on 07/03/2026 00:08 using IntelliJ IDEA
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+// Created by Abhinav Bhardwaj on 06/03/2026 00:08 using IntelliJ IDEA
 
 public class Solution {
+  public List<Boolean> kidsWithCandies(int[] candies, int extraCandies) {
+    List<Boolean> result = new ArrayList<>();
+    int maxCandies = Arrays.stream(candies).max().getAsInt();
+
+    for (int candy : candies) {
+      result.add(candy + extraCandies >= maxCandies);
+    }
+
+    return result;
+  }
 }
