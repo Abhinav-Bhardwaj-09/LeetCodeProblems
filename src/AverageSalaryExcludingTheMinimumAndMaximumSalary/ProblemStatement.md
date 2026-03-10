fb51de84527b18e09413d978c897d80ddb19e8be
Abhinav Bhardwaj <abhinavbhardwaj68@gmail.com>
2026-03-10 15:23:00 +0530

    1491. Average Salary excluding the Minimum and Maximum Salary - Reformatted files

--- src/AverageSalaryExcludingTheMinimumAndMaximumSalary/ProblemStatement.md
+++ src/AverageSalaryExcludingTheMinimumAndMaximumSalary/ProblemStatement.md
@@ -1,7 +1,7 @@
 # Problem Statement
     You are given an array of unique integers salary where salary[i] is the salary of the ith employee.
 
-    Return the average salary of employees excluding the minimum and maximum salary.
+    Return the average salary of employees excluding the minimum and maximum salary. Answers within (10)^-5 of the actual answer will be accepted.
 
 ## Example 1:
 
@@ -16,4 +16,10 @@
     Output: 2000.00000
     Explanation: Minimum salary and maximum salary are 1000 and 3000 respectively.
                  Average salary excluding minimum and maximum salary is (2000) / 1 = 2000
+
+## Constraints:
+
+- 3 <= salary.length <= 100
+- 1000 <= salary[i] <= 10^6
+- All the integers of salary are unique.
     
--- src/AverageSalaryExcludingTheMinimumAndMaximumSalary/Solution.java
+++ src/AverageSalaryExcludingTheMinimumAndMaximumSalary/Solution.java
@@ -2,23 +2,17 @@ package AverageSalaryExcludingTheMinimumAndMaximumSalary;
 
 import java.util.Arrays;
 
-/**
- * Created by IntelliJ IDEA
- * User: Abhinav Bhardwaj
- * Date: 24/01/23
- * Time: 15:10
- */
-
+// Created by Abhinav Bhardwaj on 24/01/23 15:10 using IntelliJ IDEA
 
 public class Solution {
-    public double average(int[] salary) {
-        Arrays.sort(salary);
+  public double average(int[] salary) {
+    Arrays.sort(salary);
 
-        double sum = 0, len = salary.length;
-        for (int index = 1; index < len - 1; index++) {
-            sum += salary[index];
-        }
-
-        return sum / (len - 2);
+    double sum = 0, len = salary.length;
+    for (int index = 1; index < len - 1; index++) {
+      sum += salary[index];
     }
+
+    return sum / (len - 2);
+  }
 }
